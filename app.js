44738cd760509ea9e7f122704de6e15fd7e37557
sumanthyarajala <S556122@nwmissouri.edu>
2023-04-21 12:11:32 -0500

    updste accout.js

--- app.js
+++ app.js
@@ -27,6 +27,22 @@ var eraserRouter = require('./routes/eraser');
 var boardRouter = require('./routes/board');
 var selectorRouter = require('./routes/selector');
 var resourceRouter = require('./routes/resource');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+  Account.findOne({ username: username }, function (err, user) {
+  if (err) { return done(err); }
+  if (!user) {
+  return done(null, false, { message: 'Incorrect username.' });
+  }
+  if (!user.validPassword(password)) {
+  return done(null, false, { message: 'Incorrect password.' });
+  }
+  return done(null, user);
+  });
+  }))
+
 
 
 var app = express();
@@ -47,6 +63,13 @@ app.use('/eraser', eraserRouter);
 app.use('/board', boardRouter);
 app.use('/selector', selectorRouter);
 app.use('/resource', resourceRouter);
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+  }));
+  app.use(passport.initialize());
+  app.use(passport.session());
 
 async function recreateDB(){
   // Delete everything
@@ -74,6 +97,14 @@ async function recreateDB(){
 }
  let reseed = true;
  if (reseed) { recreateDB();}
+ // passport config
+// Use the existing connection
+// The Account model
+var Account =require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+ 
 
 
 // catch 404 and forward to error handler
